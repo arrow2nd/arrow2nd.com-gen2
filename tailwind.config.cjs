83bd9377f3b5381705956968910ca1bc94d46989
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-06-11 18:23:00 +0900

    🔥 不要なカスタム定義を削除

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,12 +9,7 @@ module.exports = {
       colors: {
         main: "#434343",
         background: "#FBFBFB",
-        black: "#1C1C1C",
         sea: "#45A3CC",
-      },
-      letterSpacing: {
-        0.2: ".2rem",
-        0.4: ".4rem",
       },
     },
   },
