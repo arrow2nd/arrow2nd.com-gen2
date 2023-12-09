c00465c0231fa25d5a525d9456728a14d89f82cf
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-12-09 14:38:11 +0900

    🔧 astro-compressを追加
    astro-iconがsvgo@3に対応できておらずエラーになるので、一旦2.8で固定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), compress()]
 });
+
