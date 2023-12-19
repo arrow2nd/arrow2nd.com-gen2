e79a04677c18589c0e4d51645b61c6ffd646f898
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-12-19 21:55:34 +0900

    ♻️ 型を調整

--- src/types/category.ts
+++ src/types/category.ts
@@ -1,3 +1,5 @@
+import type { Content } from "newt-client-js";
+
 export type Category = {
   name: string;
-};
+} & Content;
--- src/types/section.ts
+++ src/types/section.ts
@@ -1,4 +1,6 @@
+import type { Content } from "newt-client-js";
+
 export type Section = {
   title: string;
   text: string;
-};
+} & Content;
