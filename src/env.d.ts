7250885f508b01f5089077edf7a6ea79f15c875c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-01-13 16:58:22 +0900

    ✨ 検索機能の追加

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,3 +5,9 @@ interface ImportMetaEnv {
   readonly NEWT_SPACE_UID: string;
   readonly NEWT_CDN_API_TOKEN: string;
 }
+
+declare module "@pagefind/default-ui" {
+  declare class PagefindUI {
+    constructor(arg: any);
+  }
+}
