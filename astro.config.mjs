2021ddcffd8cfdc635a961f1693889fdaabf2bc6
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-01-13 21:59:58 +0900

    👍 sitemapの生成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-
 import compress from "astro-compress";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(), icon()],
+  integrations: [tailwind(), compress(), icon(), sitemap()],
+  site: "https://www.arrow2nd.com",
   image: {
-    remotePatterns: [{ protocol: "https" }]
+    remotePatterns: [
+      {
+        protocol: "https"
+      }
+    ]
   }
 });
 
