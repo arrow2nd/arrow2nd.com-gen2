3bd287c76f3912cfa6d59f2f53d2d31523197a7b
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-07-19 10:39:48 +0900

    📦 Astro 5.12に

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,56 +1,34 @@
-import { file } from "astro/loaders";
+import { file, glob } from "astro/loaders";
 import { defineCollection } from "astro:content";
 import { z } from "astro:content";
 import { SectionSchema } from "./types/section";
 import { CategorySchema } from "./types/category";
 import { CareerSchema } from "./types/career";
 import { WorkSchema } from "./types/work";
-import { parse } from "toml";
 
 const about = defineCollection({
-  loader: file("src/data/about.toml", {
-    parser: (text) => parse(text),
-  }),
-  schema: z.array(SectionSchema),
+  loader: file("src/data/about.toml"),
+  schema: z.array(SectionSchema)
 });
 
 const careers = defineCollection({
-  loader: file("src/data/careers.toml", {
-    parser: (text) => parse(text),
-  }),
-  schema: z.array(CareerSchema),
+  loader: file("src/data/careers.toml"),
+  schema: z.array(CareerSchema)
 });
 
 const categories = defineCollection({
-  loader: file("src/data/categories.toml", {
-    parser: (text) => parse(text),
-  }),
-  schema: z.array(CategorySchema),
+  loader: file("src/data/categories.toml"),
+  schema: z.array(CategorySchema)
 });
 
 const works = defineCollection({
-  loader: async () => {
-    const files = import.meta.glob<string>("src/data/works/**/*.toml", {
-      query: "?raw",
-      import: "default",
-      eager: true,
-    });
-
-    const entries = [];
-
-    for (const [_, content] of Object.entries(files)) {
-      const parsed = parse(content);
-      entries.push(parsed);
-    }
-
-    return entries;
-  },
-  schema: WorkSchema,
+  loader: glob({ pattern: "**/*.toml", base: "src/data/works" }),
+  schema: WorkSchema
 });
 
 export const collections = {
   about,
   careers,
   categories,
-  works,
+  works
 };
